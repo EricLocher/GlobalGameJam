857368886187ffc20da5c1b79e7063386ca1ddba
PepperRoni <90191976+PepperRoni@users.noreply.github.com>
2022-01-28 19:06:38 +0100

    Updates

--- Game/GlobalGameJam/Assets/Scenes/PlayerController.cs
+++ Game/GlobalGameJam/Assets/Scenes/PlayerController.cs
@@ -4,5 +4,63 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] LayerMask ignoreLayer;
 
+    Rigidbody2D rb;
+    float speed = 5;
+    float jumpForce = 5;
+
+    public bool isGrounded = false;
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+
+    private void Start()
+    { 
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move();
+        Jump();
+        GroundChecker();
+    }
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = x * speed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void GroundChecker()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1, ignoreLayer);
+
+
+        if (hit.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+            isGrounded = false;
+    }
 }
