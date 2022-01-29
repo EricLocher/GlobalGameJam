b58b7be829bd935e5080208e3c26d51bd0d8bdb8
EricLocher <eriloc1129@skola.goteborg.se>
2022-01-29 12:47:13 +0100

    platform move

--- Game/GlobalGameJam/Assets/movingPlatform.cs
+++ Game/GlobalGameJam/Assets/movingPlatform.cs
@@ -5,4 +5,74 @@ using UnityEngine;
 public class movingPlatform : MonoBehaviour
 {
 
+    [SerializeField] MoveDirections moveDir = MoveDirections.Vertical;
+    [SerializeField] float moveDist;
+    [SerializeField] float moveSpeed;
+
+    Vector2 startPos;
+    bool check = false;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+
+    void Update()
+    {
+        
+        if(moveDir == MoveDirections.Vertical)
+        {
+            if(Mathf.Abs(transform.position.y - startPos.y) >= moveDist)
+            {
+                if(check) { check = false; }
+                else { check = true; }
+            }
+
+            float moveY = (moveDist * (Time.deltaTime * moveSpeed));
+
+            if (check)
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y + moveY);
+            }
+            else
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y - moveY);
+            }
+
+
+        }
+        else
+        {
+            if (Mathf.Abs(transform.position.x - startPos.x) >= moveDist)
+            {
+                if (check) { check = false; }
+                else { check = true; }
+            }
+
+            float moveX = (moveDist * (Time.deltaTime * moveSpeed));
+
+            if (check)
+            {
+                transform.position = new Vector2(transform.position.x + moveX, transform.position.y);
+            }
+            else
+            {
+                transform.position = new Vector2(transform.position.x - moveX, transform.position.y);
+            }
+        }
+        
+
+
+    }
+
+    enum MoveDirections
+    {
+        Vertical,
+        Horizontal
+    }
+
+
 }
+
+
