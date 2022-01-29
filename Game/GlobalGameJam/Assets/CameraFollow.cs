14cfe8ee08272266085b80140029d5342cb90f8d
EricLocher <eriloc1129@skola.goteborg.se>
2022-01-29 23:36:04 +0100

    My name is bond, Camera Bond

--- Game/GlobalGameJam/Assets/CameraFollow.cs
+++ Game/GlobalGameJam/Assets/CameraFollow.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
@@ -6,6 +7,21 @@ public class CameraFollow : MonoBehaviour
     Transform target;
     public float lerpSpeed = 0.125f;
     public bool followTarget = true;
+    Camera MainCam;
+    Vector3 targetPos;
+    [SerializeField] CameraBounds cameraBounds;
+
+    float cameraWidth;
+    float cameraHeight;
+
+    void Start()
+    {
+        MainCam = Camera.main;
+
+        cameraHeight = MainCam.orthographicSize;
+        cameraWidth = MainCam.aspect * cameraHeight;
+    }
+
 
     void FixedUpdate()
     {
@@ -14,11 +30,44 @@ public class CameraFollow : MonoBehaviour
 
         if (followTarget == true)
         {
-            Vector3 targetPos = new Vector3(target.position.x, target.position.y, -10);
+
+            if(
+                target.position.x - cameraWidth < cameraBounds.pos.x - cameraBounds.size.x/2
+                ||
+                target.position.x + cameraWidth > cameraBounds.pos.x + cameraBounds.size.x/2
+                ||
+                target.position.y - cameraHeight < cameraBounds.pos.y - cameraBounds.size.y/2
+                ||
+                target.position.y + cameraHeight > cameraBounds.pos.y + cameraBounds.size.y/2
+              )
+            {
+
+            }
+            else
+            {
+                targetPos = new Vector3(target.position.x, target.position.y, -10);
+            }
+
+            targetPos.z = -10;
+
             Vector3 lerpPos = Vector3.Lerp(transform.position, targetPos, lerpSpeed * Time.fixedDeltaTime);
             transform.position = lerpPos;
 
         }
     }
 
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(cameraBounds.pos, cameraBounds.size);
+    }
+
+}
+
+[Serializable]
+class CameraBounds
+{
+    public Vector2 pos;
+    public Vector2 size;
 }
