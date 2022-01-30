98c15bd67cb1188e38522377b90b76b78eaa9315
EricLocher <eriloc1129@skola.goteborg.se>
2022-01-30 14:41:47 +0100

    Spoikes

---
+++ Game/GlobalGameJam/Assets/Spikes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+            Level.ResetLevel();
+    }
+}
